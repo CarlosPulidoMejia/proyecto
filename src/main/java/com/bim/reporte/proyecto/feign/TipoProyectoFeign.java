bcca3398811e23052a0a2190c5558b1b8e915dc9
CarlosPulidoMejia <cjlpulido@hotmail.com>
2024-02-28 17:22:24 -0600

    corrección feign

--- src/main/java/com/bim/reporte/proyecto/feign/TipoProyectoFeign.java
+++ src/main/java/com/bim/reporte/proyecto/feign/TipoProyectoFeign.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bim.reporte.proyecto.request.DetalleDocumentacionRequest;
 import com.bim.reporte.proyecto.request.DetalleEstadoProyectoRequest;
